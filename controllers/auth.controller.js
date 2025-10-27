19c14c8baf17ed3e22056e4bcf9852ce9245e927
ansbajaj69 <bajajansh822@gmail.com>
2025-10-27 17:25:05 +0530

    1-project setup

---
+++ controllers/auth.controller.js
@@ -0,0 +1,35 @@
+export const signup = async (req,res)=>{
+    try{
+
+    }
+    catch(error){
+
+    }
+}
+
+export const login = async (req,res)=>{
+    try{
+
+    }
+    catch(error){
+        
+    }
+}
+
+export const getUserProfile = async (req,res)=>{
+    try{
+
+    }
+    catch(error){
+        
+    }
+}
+
+export const fetchFriends = async (req,res)=>{
+    try{
+
+    }
+    catch(error){
+        
+    }
+}
